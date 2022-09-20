1a30af4235098700d4a976cb2abd4657adb13fbd
Алексей Туманян <philotelist@rambler.ru>
2022-09-20 10:15:23 +0300

    tests

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,9 +4,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ..api.pagination import CustomPageNumberPagination
-from .models import Follow, User
-from .serializers import FollowListSerializer, FollowSerializer
+from api.pagination import CustomPageNumberPagination
+from users.models import Follow, User
+from users.serializers import FollowListSerializer, FollowSerializer
 
 
 class FollowApiView(APIView):
