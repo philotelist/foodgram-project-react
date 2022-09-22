0bf1eeb00aa1f6cae74a35832bf2a4eae42fdb76
Алексей Туманян <philotelist@rambler.ru>
2022-09-22 17:31:52 +0300

    tests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': [
         'rest_framework.pagination.PageNumberPagination',
-        'PAGE_SIZE': 6
+        'PAGE_SIZE': 6,
     ],
 }
 
