ffc9e2137c81613f47506b53d6e28a2080042428
Алексей Туманян <philotelist@rambler.ru>
2022-09-22 21:40:01 +0300

    tests

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -6,11 +6,11 @@ urlpatterns = [
     path(
         'users/<int:id>/subscribe/', FollowApiView.as_view(),
         name='subscribe'
-     ),
+         ),
     path(
         'users/subscriptions/', FollowListAPIView.as_view(),
         name='subscription'
-     ),
+         ),
     path('auth/', include('djoser.urls.authtoken')),
     path('', include('djoser.urls')),
 ]
