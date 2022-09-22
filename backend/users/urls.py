42d13d5e501d9909fc2dfafac566cf0eee4c7f17
Алексей Туманян <philotelist@rambler.ru>
2022-09-22 19:55:30 +0300

    tests

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,11 +4,13 @@ from users.views import FollowApiView, FollowListAPIView
 
 urlpatterns = [
     path(
-        'users/<int:id>/subscribe/', FollowApiView.as_view(),
-         name='subscribe'
+        'users/<int:id>/subscribe/',
+        FollowApiView.as_view(),
+        name='subscribe'
     ),
     path(
-         'users/subscriptions/', FollowListAPIView.as_view(),
+         'users/subscriptions/',
+         FollowListAPIView.as_view(),
          name='subscription'
     ),
     path('auth/', include('djoser.urls.authtoken')),
