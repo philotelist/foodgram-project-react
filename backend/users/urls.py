9cc0e57bd225f4e5a1b9c2947159fbbe5556849d
Алексей Туманян <philotelist@rambler.ru>
2022-09-22 21:19:32 +0300

    tests

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,10 +3,8 @@ from django.urls import include, path
 from users.views import FollowApiView, FollowListAPIView
 
 urlpatterns = [
-     path('users/<int:id>/subscribe/', FollowApiView.as_view(),
-                                             name='subscribe'),
-     path('users/subscriptions/', FollowListAPIView.as_view(),
-                                         name='subscription'),
+     path('users/<int:id>/subscribe/', FollowApiView.as_view(), name='subscribe'),
+     path('users/subscriptions/', FollowListAPIView.as_view(), name='subscription'),
      path('auth/', include('djoser.urls.authtoken')),
      path('', include('djoser.urls')),
 ]
