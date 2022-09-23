57e36cb734d4fd3c151a0faec0ffd3c35a577f49
Алексей Туманян <philotelist@rambler.ru>
2022-09-23 03:45:34 +0300

    tests

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -5,9 +5,8 @@ def ingredients_list(self, request):
     ingredients = IngredientQuantity.objects.filter(
         recipe__shopping_carts__user=request.user).values(
             'ingredient__name', 'ingredient__measurement_unit', 'amount')
-    return ('\n'.join(
-        [f'{ingredient["ingredient__name"]} - {ingredient["amount"]}'
-                      f'{ingredient["ingredient__measurement_unit"]}'
-                      for ingredient in ingredients]
-            )
+    return ('\n'.join([
+                f'{ingredient["ingredient__name"]} - {ingredient["amount"]}'
+                f'{ingredient["ingredient__measurement_unit"]}'
+                for ingredient in ingredients])
     )
