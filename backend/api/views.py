a807d1cb06d90b125be773ddd50ef0a2f1505409
Алексей Туманян <philotelist@rambler.ru>
2022-09-23 14:13:09 +0300

    tests

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,9 +4,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import (
-                                        AllowAny,
-                                        IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly
+                                    AllowAny,
+                                    IsAuthenticated,
+                                    IsAuthenticatedOrReadOnly
 )
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
