df09741e32aceb837c7ac9809daa434dfc16b00b
Алексей Туманян <philotelist@rambler.ru>
2022-09-24 19:41:26 +0300

    tests

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -8,6 +8,7 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'email', 'first_name', 'last_name')
     search_fields = ('username', 'email')
     list_filter = ('username', 'email')
+    readonly_fields = ('последний вход', 'дата регистрации')
 
 
 @admin.register(Follow)
