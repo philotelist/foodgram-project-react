7cc06507cff96a114981e56f6b73a6f99be37030
Алексей Туманян <philotelist@rambler.ru>
2022-09-24 23:02:22 +0300

    tests

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -14,7 +14,7 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'measurement_unit')
-    list_filter = ('name',)
+    # list_filter = ('name',)
     search_fields = ('name',)
 
 
