e776a7ce3279f310027160d63a06a81fd529cbce
Алексей Туманян <philotelist@rambler.ru>
2022-09-30 12:30:38 +0300

    tests

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,31 +1,34 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
+# from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+User = get_user_model()
 
-class User(AbstractUser):
-    email = models.EmailField(
-        max_length=150, unique=True, verbose_name='Почта'
-    )
-    username = models.CharField(
-        max_length=150, unique=True, verbose_name='Имя пользователя'
-    )
-    first_name = models.CharField(
-        max_length=150, verbose_name='Имя'
-    )
-    last_name = models.CharField(
-        max_length=150, verbose_name='Фамилия'
-    )
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+# class User(AbstractUser):
+#     email = models.EmailField(
+#         max_length=150, unique=True, verbose_name='Почта'
+#     )
+#     username = models.CharField(
+#         max_length=150, unique=True, verbose_name='Имя пользователя'
+#     )
+#     first_name = models.CharField(
+#         max_length=150, verbose_name='Имя'
+#     )
+#     last_name = models.CharField(
+#         max_length=150, verbose_name='Фамилия'
+#     )
 
-    class Meta:
-        ordering = ['-id']
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
-    def __str__(self):
-        return self.username
+#     class Meta:
+#         ordering = ['-id']
+#         verbose_name = 'Пользователь'
+#         verbose_name_plural = 'Пользователи'
+
+#     def __str__(self):
+#         return self.username
 
 
 class Follow(models.Model):
