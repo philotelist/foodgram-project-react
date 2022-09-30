ad427e074ca9773524e8746ead9093eab403f499
Алексей Туманян <philotelist@rambler.ru>
2022-09-30 12:12:07 +0300

    tests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from datetime import timedelta
+# from datetime import timedelta
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
