43399c3a2ad4d812ae548ef0e86be06894f575f0
Алексей Туманян <philotelist@rambler.ru>
2022-10-02 11:38:26 +0300

    tests

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,4 @@
 # from django.db.models import Sum
-from http import HTTPStatus
-
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
