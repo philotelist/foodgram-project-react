3cbe7a04afe30db81675c079e09fd0860b75ac72
Алексей Туманян <philotelist@rambler.ru>
2022-10-02 13:34:13 +0300

    tests

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -5,10 +5,10 @@ from .models import IngredientQuantity
 
 def get_cart(request):
     ingredients = IngredientQuantity.objects.filter(
-            recipe__shopping_carts__user=request.user).values(
-            'ingredient__name',
-            'ingredient__measurement_unit',).annotate(
-            amount=Sum('amount')).order_by()
+        recipe__shopping_carts__user=request.user).values(
+        'ingredient__name',
+        'ingredient__measurement_unit',).annotate(
+        amount=Sum('amount')).order_by()
     return (
         '\n'.join([
             f'{ingredient["ingredient__name"]} - {ingredient["amount"]} '
